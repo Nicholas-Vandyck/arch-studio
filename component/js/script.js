ddf24f6d37ecec05c98f49f0076f0df2a02f36c4
NeonHayford <hayford.lumorvi@gmail.com>
2023-01-25 16:36:25 +0000

    working on the mobile view

---
+++ component/js/script.js
@@ -0,0 +1,25 @@
+// const navigate= document.querySelector(".btn-links");
+
+
+// function display() {
+//     const hamburger= document.querySelector(".hamburger-icon");
+// if(!hamburger.classList.contains("active")){
+//     hamburger.classList.add("active")
+// }   
+// }
+
+// hamburger.addEventListener(
+//     "click",()=> {
+//         hamburger.classList.toggle("active");
+//         navigate.classList.toggle("active");
+//     }
+// )
+
+// document.querySelectorAll(".btn-links").forEach(
+//     n => n.addEventListener(
+//         "click",()=> {
+//             hamburger.classList.remove("active");
+//             navigate.classList.remove("active");
+//         }
+//     )
+// )
