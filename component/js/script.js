30296fc0c8cc2447b6d93832bb7513ed555b575f
NeonHayford <hayford.lumorvi@gmail.com>
2023-01-26 16:37:40 +0000

    commit

--- component/js/script.js
+++ component/js/script.js
@@ -1,3 +1,36 @@
+  const ham = document.querySelector(".hamburger-icon");
+  // console.log(ham);
+  ham.addEventListener("click", display);
+
+  function display(){
+    const navbar = document.querySelector(".btn-links");
+    const close = document.querySelector(".close-icon");
+    navbar.classList.add("active");
+    ham.classList.add("inactive");
+    close.classList.remove("inactive");
+  }
+
+  const closebtn= document.querySelector('.close-icon');
+  closebtn.addEventListener("click", closeDisplay);
+
+  function closeDisplay() {
+    const bar = document.querySelector(".btn-links");
+    const hamburg = document.querySelector(".hamburger-icon");
+    bar.classList.remove("active");
+    closebtn.classList.add("inactive");
+    hamburg.classList.remove("inactive");
+  }
+
+
+
+
+
+
+
+
+
+
+
 // const navigate= document.querySelector(".btn-links");
 
 
